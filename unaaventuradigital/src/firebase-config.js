40963cd033197991eb15d237cb079d440737dcbe
Edwar421 <421edwar@gmail.com>
2024-07-30 00:08:05 -0500

    Firebase

---
+++ unaaventuradigital/src/firebase-config.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default{
+    apiKey: "AIzaSyCDD4B-tANRwIcD4NiBN0qvKZOIoULQgBI",
+  authDomain: "una-avetura-digital.firebaseapp.com",
+  projectId: "una-avetura-digital",
+  storageBucket: "una-avetura-digital.appspot.com",
+  messagingSenderId: "910980658235",
+  appId: "1:910980658235:web:ec7e34f949ed42f33bd81e",
+  measurementId: "G-XC2QTQR0VY"
+}
--- unaaventuradigital/src/index.js
+++ unaaventuradigital/src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebaseConfig from './firebase-config';
+import { FirebaseAppProvider } from 'reactfire';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  < FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <Suspense fallback={'Conectando a Firebase'}> 
+      <App />
+    </Suspense>
+  </FirebaseAppProvider>
   </React.StrictMode>
 );
 
